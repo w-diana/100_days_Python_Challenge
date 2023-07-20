94336362491515668cba75fd35665f0ea86ee049
Wapei Diana Wadawasina <122484745+w-diana@users.noreply.github.com>
2023-07-20 03:36:47 -0400

    Add files via upload

---
+++ Day 3 - Treasure Island/main.py
@@ -0,0 +1,43 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+action = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" ')
+
+if action.lower() == "left":
+    action = input('You\'re infront of a broken bridge being repaired. What do you want to do? Type "swim" or "wait" ')
+    if action.lower() == "wait":
+        action = input('Three magical doors appeared. Which door do you want to enter? Type "blue" or "red" or "yellow" ')
+        if action.lower() == "yellow":
+            print("You found the treasure! You Win!")
+        elif action.lower() == "red":
+            print("You entered into a trap. Game Over.")
+        else:
+            print("Its a room full of dragons. A dragon grilled you. Game Over.")
+    else: 
+        print("You got eaten by a crocodile. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
