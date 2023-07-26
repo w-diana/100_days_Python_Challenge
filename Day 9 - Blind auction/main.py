2a9cfbde9f50424bb90cc5943d4c184938832bad
Wapei Diana Wadawasina <122484745+w-diana@users.noreply.github.com>
2023-07-25 22:41:14 -0400

    Add files via upload

--- Day 9 - Blind auction/main.py
+++ Day 9 - Blind auction/main.py
@@ -1,1 +1,29 @@
+from replit import clear
+from art import logo
+#HINT: You can call clear() to clear the output in the console.
+def add_bid(name, bid, bidders):
+  bidders[name] = bid
 
+def find_highest_bidder(bidders):
+  max = 0 
+  for bidder in bidders:
+    if bidders[bidder] > max:
+      max_bid = bidders[bidder]
+      max_bidder = bidder
+  print(f"The winner is {max_bidder} with a bid of ${max_bid}")
+
+print(logo)
+print("Welcome to the secret auction program. ")
+
+more_bidders = "yes"
+bidders = {}
+
+while more_bidders == "yes":
+  name = input("What is your name?: ")
+  bid = int(input("What's your bid?: $"))
+  add_bid(name, bid, bidders)
+  more_bidders = input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+  if more_bidders == "yes": 
+    clear()
+  else:
+    find_highest_bidder(bidders)
