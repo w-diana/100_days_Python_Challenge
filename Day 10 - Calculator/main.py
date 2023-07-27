25321d6741d0cc09b5020ad269235e6a866414ed
Wapei Diana Wadawasina <122484745+w-diana@users.noreply.github.com>
2023-07-26 22:16:10 -0400

    Add files via upload

--- Day 10 - Calculator/main.py
+++ Day 10 - Calculator/main.py
@@ -1,1 +1,51 @@
+from art import logo 
+from replit import clear
 
+# Add
+def add(n1, n2):
+  return n1 + n2
+  
+# Subtract
+def subtract(n1, n2):
+  return n1 - n2
+  
+# Multiply
+def multiply(n1, n2):
+  return n1 * n2
+  
+# Divide
+def divide(n1, n2):
+  return n1/n2
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What's the first number? : "))
+  
+  should_continue = True
+  while should_continue:
+    for operation in operations:
+      print(operation)
+
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number? : "))
+  
+    answer = operations[operation_symbol](num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    continue_operation = input(f"Type 'y' to continue calculating with {answer}  or type 'n' to exit.: ").lower()
+    
+    if continue_operation == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      clear()
+      calculator()
+
+operations = {
+  "+" : add,
+  "-" : subtract,
+  "*" : multiply,
+  "/" : divide
+}
+
+calculator()
