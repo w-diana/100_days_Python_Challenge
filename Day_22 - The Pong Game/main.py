4063db531874e8b3eba1ed5d5ec94b4f00131482
Wapei Diana Wadawasina <122484745+w-diana@users.noreply.github.com>
2023-10-20 01:20:30 -0400

    Add files via upload
    Added all the files

---
+++ Day_22 - The Pong Game/main.py
@@ -0,0 +1,53 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+right_paddle = Paddle((350,0))
+left_paddle = Paddle((-350,0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)  # To slow the screen update so ball not moving too fast
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor()>320 or ball.distance(left_paddle) < 50 and ball.xcor()<-320:
+        ball.bounce_x()
+
+    # Detect right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.left_point()
+
+    # Detect left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.right_point()
+
+
+
+
+
+
+screen.exitonclick()
